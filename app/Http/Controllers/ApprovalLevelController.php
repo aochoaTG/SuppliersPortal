e1f50cbe09b6e16bb1e3cdc6cff4f9cfac05369c
aochoaTG <aldo.ochoa@totalgas.com>
2026-02-17 10:30:26 -0700

    PDP2026-1: Prueba de vinculación con Jira

--- app/Http/Controllers/ApprovalLevelController.php
+++ app/Http/Controllers/ApprovalLevelController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\ApprovalLevel;
 use Illuminate\Http\Request;
 use App\Services\ApprovalService;
+use Illuminate\Support\Facades\Auth;
 
 class ApprovalLevelController extends Controller
 {
