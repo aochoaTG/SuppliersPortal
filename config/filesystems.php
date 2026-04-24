6ef85df3107c056594d77d9635dfe9a2c736a072
aochoaTG <aldo.ochoa@totalgas.com>
2026-04-23 18:34:46 -0600

    Archivos en carpeta compartida

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,7 +40,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => env('STORAGE_PUBLIC_PATH', storage_path('app/public')),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
@@ -78,7 +78,7 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('app/public'),
+        public_path('storage') => env('STORAGE_PUBLIC_PATH', storage_path('app/public')),
     ],
 
 ];
